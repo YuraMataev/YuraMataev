3fcf801d165fb51df89de23a4a81a7cbf73c5850
Yura Mataev <Mataev_91@outlook.com>
2022-04-04 00:56:53 +0300

    1 Урок ДЗ

---
+++ Урок 1. Основы языка JavaScript/app.js
@@ -0,0 +1,7 @@
+
+alert("°C в °F  - Перевод Цельсия  в Фаренгейта.\n Нажмите Ок чтобы начать!")
+var userAnwer = +prompt("Введите значения температуру в ℃");
+var celsus = userAnwer;
+var farengeit = celsus * 1.8 + 32;
+
+alert(farengeit + " ℉");
